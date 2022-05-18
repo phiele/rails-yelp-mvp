4759a3b44dac8c742762133f6a8802e233f832a8
Philipp Riedel <riedel.philipp@gmx.net>
2022-05-18 16:56:37 +0200

    creates controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "restaurants#index"
-  resources :restaurants do
-    resources :reviews
+  # root 'restaurants#index'
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
   end
 end
